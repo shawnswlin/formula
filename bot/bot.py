e46dc917e84648e6dfaa3f946aabe6cab370bb7c
shawnswlin <shawnswlin@gmail.com>
2018-09-06 10:51:42 +0800

    upgrade to v2

--- bot/bot.py
+++ bot/bot.py
@@ -15,6 +15,12 @@ from keras.models import load_model
 sio = socketio.Server()
 
 
+class Status(object):
+    RUNNING = 0
+    FINISHED = 1  # All laps completed, the connection will be terminated soon.
+    TIMEOUT = 2  # Timeout limit reached, the connection will be terminated soon.
+
+
 @sio.on('telemetry')
 def telemetry(sid, data):
     # The current steering angle of the car
@@ -25,6 +31,14 @@ def telemetry(sid, data):
     speed = float(data["speed"])
     # The current image from the center camera of the car
     imgString = data["image"]
+    elapsed = float(data["time"])
+    status = int(data["status"])
+
+    if elapsed > 600 or status in [Status.FINISHED, Status.TIMEOUT]:
+        print("Elapsed {} seconds. Restart game.".format(elapsed))
+        send_restart()
+        return
+
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
     transformed_image_array = image_array[None, :, :, :]
@@ -33,9 +47,10 @@ def telemetry(sid, data):
     predictions = model.predict(transformed_image_array, batch_size=1)
     steering_angle = float(predictions[0][0])
 
-    # Use rule-based throttle per steering_angle
+    # Use rule-based throttle per steering_angle.
+    # For Track 1
     if abs(steering_angle) > 5.0:
-        throttle = 0.001
+        throttle = 0.005
     else:
         throttle = max(0.01, -0.15 / 0.05 * abs(steering_angle) + 0.35)
 
@@ -51,9 +66,13 @@ def connect(sid, environ):
 def send_control(steering_angle, throttle):
     print(steering_angle, throttle)
     sio.emit("steer", data={
-        'steering_angle': steering_angle.__str__(),
-        'throttle': throttle.__str__()
+        'steering_angle': str(steering_angle),
+        'throttle': str(throttle)
     }, skip_sid=True)
+
+
+def send_restart():
+    sio.emit("restart", data={}, skip_sid=True)
 
 
 if __name__ == '__main__':
